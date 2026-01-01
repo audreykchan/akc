82a051f5b6ab51ec3280517e25c80ad738f9f05b
donaldide1232321 <audreykc144@gmail.com>
2025-12-31 21:57:31 -0500

    a

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+const base = process.env.GH_PAGES_BASE ?? (process.env.CI ? './' : '/');
+
 export default defineConfig({
+  base,
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
